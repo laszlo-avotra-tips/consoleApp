3af2bda703dbce2bc157ca360407d6a42d2d57bc
Laszlo Vincze <lvincze@avinger.local>
2020-05-29 08:51:29 -0700

    21272 - added date and time reading

--- Console/consoleApp/Frontend/Widgets/dialogcaseinformation.cpp
+++ Console/consoleApp/Frontend/Widgets/dialogcaseinformation.cpp
@@ -1,11 +1,14 @@
 #include "dialogcaseinformation.h"
 #include "ui_dialogcaseinformation.h"
+#include <QDateTime>
 
 DialogCaseInformation::DialogCaseInformation(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogCaseInformation)
 {
     ui->setupUi(this);
+    setWindowFlags(Qt::SplashScreen);
+    ui->lineEditDateAndTime->setText(QDateTime::currentDateTime().toString("yyyy-MM-dd  hh:mm:ss"));
 }
 
 DialogCaseInformation::~DialogCaseInformation()
