c1b0d376c3774d449e19e244096ba99f4461538b
Laszlo Vincze <lvincze@avinger.local>
2020-08-09 07:58:40 -0700

    0.1.1333 - force trigger count while retrieving image

--- Console/consoleApp/Backend/daq.cpp
+++ Console/consoleApp/Backend/daq.cpp
@@ -242,15 +242,15 @@ bool DAQ::getData( )
         isReturn = true;
     }
 
-    if(force_trig != sforce_trig){
-        sforce_trig = force_trig;
-        if( force_trig == 1){
-            ++force_trigCount;
-        }
-    }
-    if( force_trig == 1){
-        isReturn = true;
-    }
+//    if(force_trig != sforce_trig){
+//        sforce_trig = force_trig;
+//        if( force_trig == 1){
+//            ++force_trigCount;
+//        }
+//    }
+//    if( force_trig == 1){
+//        isReturn = true;
+//    }
 
     if(required_buffer_size >= MAX_ACQ_IMAGE_SIZE){
         QString errorMsg("required_buffer_size >= myBufferSize");
