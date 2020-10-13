2165a9e2356abe1deefa44bd3f4faa2c99d36b1c
Laszlo Vincze <lvincze@avinger.local>
2020-10-13 11:16:45 -0700

    0.1.18115 - if state

--- Console/consoleApp/Frontend/Widgets/deviceDelegate.cpp
+++ Console/consoleApp/Frontend/Widgets/deviceDelegate.cpp
@@ -3,6 +3,7 @@
 #include <QPainter>
 #include <QBrush>
 #include <QPalette>
+#include <QColor>
 
 DeviceDelegate::DeviceDelegate(QObject *parent) : QItemDelegate(parent)
 {    
@@ -13,15 +14,15 @@ void DeviceDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option
 {
     painter->save();
     QStyleOptionViewItem myOption(option);
-//    if (option.state & QStyle::State_Selected){
+    if (option.state & QStyle::State_Selected){
 //        const auto& rect = option.rect;
 //        const auto& color = option.palette.highlight(); // QBrush(Qt::gray)
         QPalette myPalette(option.palette);
-        myPalette.setBrush(QPalette::Highlight,QBrush(Qt::gray));
-        myPalette.setBrush(QPalette::HighlightedText,QBrush(Qt::yellow));
+        myPalette.setBrush(QPalette::Highlight,QBrush(QColor(96,96,96)));
+//        myPalette.setBrush(QPalette::HighlightedText,QBrush(Qt::yellow));
         myOption.palette = myPalette;
 //        painter->fillRect(rect, color);
-//    }
+    }
 
     QItemDelegate::paint(painter, myOption, index);
     painter->restore();
