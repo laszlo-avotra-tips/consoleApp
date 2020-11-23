54a41669f58872c1e00332ade32e961ddf3aec38
Laszlo Vincze <lvincze@avinger.local>
2020-11-23 13:44:24 -0800

    0.2.11434 - ".../clips/LOOP..."

--- Console/consoleApp/Frontend/Utility/octFrameRecorder.cpp
+++ Console/consoleApp/Frontend/Utility/octFrameRecorder.cpp
@@ -35,7 +35,8 @@ void OctFrameRecorder::updateOutputFileName(int loopNumber)
     m_playlistFileName = m_playlistThumbnail + QString( ".m3u8" );
 //    LOG1(m_playlistFileName)
     caseInfo &info = caseInfo::Instance();
-    m_outDirPath = info.getStorageDir() + "/clips/"; // Set up the absolute path based on the session data.
+//    m_outDirPath = info.getStorageDir() + "/clips/"; // Set up the absolute path based on the session data.
+    m_outDirPath = QString("%1/clips/%2").arg(info.getStorageDir()).arg(m_playlistFileName); // Set up the absolute path based on the session data.
 }
 
 void OctFrameRecorder::updateClipList(int loopNumber)
