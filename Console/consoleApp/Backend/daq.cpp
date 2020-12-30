05fde7c0fe28a28ce966eb6b590967035a03aca4
Laszlo Vincze <lvincze@avinger.local>
2020-12-30 14:29:18 -0800

    0.2.13014 - axStartSession(&session, 5);

--- Console/consoleApp/Backend/daq.cpp
+++ Console/consoleApp/Backend/daq.cpp
@@ -359,7 +359,7 @@ bool DAQ::startDaq()
 
     try {
 
-        success = axStartSession(&session, 15);    // Start Axsun engine session
+        success = axStartSession(&session, 5);    // Start Axsun engine session
         if(success != NO_AxERROR){
             logAxErrorVerbose(__LINE__, success);
         }
