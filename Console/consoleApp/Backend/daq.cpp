01ca6434f5a251ac00b6e2093ef27c58d5f6b5a8
Laszlo Vincze <lvincze@avinger.local>
2021-01-12 03:15:12 -0800

    0.2.13202 - start over new dll

--- Console/consoleApp/Backend/daq.cpp
+++ Console/consoleApp/Backend/daq.cpp
@@ -61,7 +61,7 @@ void DAQ::logAxErrorVerbose(int line, AxErr axErrorCode)
 {
     char errorVerbose[512];
     axGetErrorString(axErrorCode, errorVerbose);
-    LOG3(line, axErrorCode, errorVerbose)
+    LOG3(line, int(axErrorCode), errorVerbose)
 }
 
 DAQ::~DAQ()
@@ -130,21 +130,21 @@ void DAQ::run( void )
         LOG2(loopCount, m_decimation)
         LOG1("***** Thread: DAQ::run()")
         retval = axOpenAxsunOCTControl(true);
-        if(retval != NO_AxERROR){
+        if(retval != AxErr::NO_AxERROR){
             logAxErrorVerbose(__LINE__, retval);
         }
 
         retval = axNetworkInterfaceOpen(1);
-        if(retval != NO_AxERROR){
+        if(retval != AxErr::NO_AxERROR){
             char errorMsg[512];
             axGetErrorString(retval, errorMsg);
-            LOG2(retval, errorMsg)
+            LOG2(int(retval), errorMsg)
         }
         retval = axUSBInterfaceOpen(1);
-        if(retval != NO_AxERROR){
+        if(retval != AxErr::NO_AxERROR){
             char errorMsg[512];
             axGetErrorString(retval, errorMsg);
-            LOG2(retval, errorMsg)
+            LOG2(int(retval), errorMsg)
         }
 
 
@@ -206,133 +206,157 @@ bool DAQ::getData( )
     float lostImagesInPercent = 0.0f;
     int32_t width = 0;
     int32_t height = 0;
-    AxDataType data_type = U8;
+    AxDataType data_type = AxDataType::U8;
     uint32_t returned_image = 0;
     uint8_t force_trig = 0;
     uint8_t trig_too_fast = 0;
     static std::map<AxErr,int> errorTable;
     static int64_t force_trigCount = 0;
     static int64_t trig_too_fastCount = 0;
-    AxErr success{NO_AxERROR};
-    static AxErr sRetVal{NO_AxERROR};
+    AxErr success{AxErr::NO_AxERROR};
+    static AxErr sRetVal{AxErr::NO_AxERROR};
     bool isReturnedImageNumberChanged{false};
 
     int64_t requestedImageNumber = -1;
 
-    try{
-        success = axGetImageInfoAdv(session, requestedImageNumber,
-                                    &returned_image_number, &height, &width, &data_type, &required_buffer_size, &force_trig, &trig_too_fast );
-    }  catch (std::exception& e) {
-       LOG1(e.what())
-       LOG2(requestedImageNumber,returned_image_number)
-       LOG2(height, width)
-       LOG2(data_type, required_buffer_size)
-       LOG2(force_trig, trig_too_fast)
-    } catch(...){
-        LOG1(__FUNCTION__)
+//    try{
+//        success = axGetImageInfoAdv(session, requestedImageNumber,
+//                                    &returned_image_number, &height, &width, &data_type, &required_buffer_size, &force_trig, &trig_too_fast );
+//    }  catch (std::exception& e) {
+//       LOG1(e.what())
+//       LOG2(requestedImageNumber,returned_image_number)
+//       LOG2(height, width)
+//       LOG2(int(data_type), required_buffer_size)
+//       LOG2(force_trig, trig_too_fast)
+//    } catch(...){
+//        LOG1(__FUNCTION__)
+//    }
+
+    //deprecated use axGetImageInfo
+    uint32_t imaging, last_packet, last_frame, last_image, dropped_packets, frames_since_sync;  // for axGetStatus()
+    request_prefs_t prefs{ };
+    image_info_t info{ };
+    int32_t counter = 0;
+
+    // get Main Image Buffer status
+    success = axGetStatus(session, &imaging, &last_packet, &last_frame, &last_image, &dropped_packets, &frames_since_sync);
+    if(success != AxErr::NO_AxERROR){
+        logAxErrorVerbose(__LINE__, success);
     }
 
-    isReturnedImageNumberChanged = (success == NO_AxERROR) && (returned_image_number != sprevReturnedImageNumber);
-    sprevReturnedImageNumber = returned_image_number;
+//    isReturnedImageNumberChanged = (success == AxErr::NO_AxERROR) && (returned_image_number != sprevReturnedImageNumber);
+//    sprevReturnedImageNumber = returned_image_number;
 
-    bool isReturn = false;
+//    bool isReturn = false;
 
-    if(success != sRetVal){
-        sRetVal = success;
-        if(success != NO_AxERROR){
-            if(auto it = errorTable.find(success) != errorTable.end()){
-                errorTable[success]++;
-            } else {
-                errorTable[success] = 1;
-            }
-            ++axErrorCount;
-            isReturn = true;
-        }
-    }
+//    if(success != sRetVal){
+//        sRetVal = success;
+//        if(success != AxErr::NO_AxERROR){
+//            if(auto it = errorTable.find(success) != errorTable.end()){
+//                errorTable[success]++;
+//            } else {
+//                errorTable[success] = 1;
+//            }
+//            ++axErrorCount;
+//            isReturn = true;
+//        }
+//    }
 
-    if(success != NO_AxERROR){
-        isReturn = true;
-    }
+//    if(success != AxErr::NO_AxERROR){
+//        isReturn = true;
+//    }
 
-    if(isReturnedImageNumberChanged){
-        if( force_trig == 1){
-            ++force_trigCount;
-        }
-        if(required_buffer_size >= MAX_ACQ_IMAGE_SIZE){
-            QString errorMsg("required_buffer_size >= myBufferSize");
-            LOG3(errorMsg,required_buffer_size, MAX_ACQ_IMAGE_SIZE);
-            isReturn = true;
-        }
-    }
+//    if(isReturnedImageNumberChanged){
+//        if( force_trig == 1){
+//            ++force_trigCount;
+//        }
+//        if(required_buffer_size >= MAX_ACQ_IMAGE_SIZE){
+//            QString errorMsg("required_buffer_size >= myBufferSize");
+//            LOG3(errorMsg,required_buffer_size, MAX_ACQ_IMAGE_SIZE);
+//            isReturn = true;
+//        }
+//    }
 
-    if( force_trig == 1){
-        isReturn = true;
-    }
+//    if( force_trig == 1){
+//        isReturn = true;
+//    }
 
-    if(isReturn){
-        return false;
-    }
+//    if(isReturn){
+//        return false;
+//    }
 
-    //initialize logging
-    if(!isLoggingInitialized){
-        lastImageIdx = returned_image_number - 1;
-        force_trigCount = 0;
-        axErrorCount = 0;
-        isLoggingInitialized = true;
-        LOG4(m_count, returned_image_number, lostImageCount, lostImagesInPercent)
-        LOG4(axErrorCount,required_buffer_size,height, width)
-        LOG2(force_trigCount, trig_too_fastCount)
-    }
+//    //initialize logging
+//    if(!isLoggingInitialized){
+//        lastImageIdx = returned_image_number - 1;
+//        force_trigCount = 0;
+//        axErrorCount = 0;
+//        isLoggingInitialized = true;
+//        LOG4(m_count, returned_image_number, lostImageCount, lostImagesInPercent)
+//        LOG4(axErrorCount,required_buffer_size,height, width)
+//        LOG2(force_trigCount, trig_too_fastCount)
+//    }
 
-    if(isReturnedImageNumberChanged ){
-        ++m_count;
-        ++imageCount;
+//    if(isReturnedImageNumberChanged ){
+//        ++m_count;
+//        ++imageCount;
 
-        if(m_decimation && (m_count % m_decimation == 0)){
-            lostImagesInPercent =  100.0f * lostImageCount / imageCount;
-            LOG4(m_count, returned_image_number, lostImageCount, lostImagesInPercent)
-            LOG4(axErrorCount,required_buffer_size,height, width)
-            LOG2(force_trigCount, trig_too_fastCount)
-            if(errorTable.size() >= 1){
-                for(auto it = errorTable.begin(); it != errorTable.end(); ++it ){
-                    AxErr error = it->first;
-                    int errorCode = int(error);
-                    auto errorCount = it->second;
-                    char errorMsg[512];
-                    axGetErrorString(error, errorMsg);
-                    LOG3(errorCode, errorCount, errorMsg)
-                }
-             }
-        }
-        if( returned_image_number > (lastImageIdx + 1) ){
-           lostImageCount += returned_image_number - lastImageIdx - 1;
-        }
+//        if(m_decimation && (m_count % m_decimation == 0)){
+//            lostImagesInPercent =  100.0f * lostImageCount / imageCount;
+//            LOG4(m_count, returned_image_number, lostImageCount, lostImagesInPercent)
+//            LOG4(axErrorCount,required_buffer_size,height, width)
+//            LOG2(force_trigCount, trig_too_fastCount)
+//            if(errorTable.size() >= 1){
+//                for(auto it = errorTable.begin(); it != errorTable.end(); ++it ){
+//                    AxErr error = it->first;
+//                    int errorCode = int(error);
+//                    auto errorCount = it->second;
+//                    char errorMsg[512];
+//                    axGetErrorString(error, errorMsg);
+//                    LOG3(errorCode, errorCount, errorMsg)
+//                }
+//             }
+//        }
+//        if( returned_image_number > (lastImageIdx + 1) ){
+//           lostImageCount += returned_image_number - lastImageIdx - 1;
+//        }
 
-        if( returned_image_number <= lastImageIdx )
-        {
-            LOG2(returned_image_number, lastImageIdx)
-            return false;
-        }
+//        if( returned_image_number <= lastImageIdx )
+//        {
+//            LOG2(returned_image_number, lastImageIdx)
+//            return false;
+//        }
 
-//        lastImageIdx = returned_image_number;
+////        lastImageIdx = returned_image_number;
 
         OCTFile::OctData_t* axsunData = SignalModel::instance()->getOctData(gFrameNumber);
 
-        success = axRequestImage( session,
-                                   returned_image_number,
-                                   &returned_image,
-                                   &height,
-                                   &width,
-                                   &data_type,
-                                   axsunData->acqData,
-                                   MAX_ACQ_IMAGE_SIZE );
-        if(success != NO_AxERROR){
-            logAxErrorVerbose(__LINE__, success);
-            return false;
-        }
-        lastImageIdx = returned_image;
+//        success = axRequestImage( session,
+//                                   returned_image_number,
+//                                   &returned_image,
+//                                   &height,
+//                                   &width,
+//                                   &data_type,
+//                                   axsunData->acqData,
+//                                   MAX_ACQ_IMAGE_SIZE );
+//        if(success != AxErr::NO_AxERROR){
+//            logAxErrorVerbose(__LINE__, success);
+//            return false;
+//        }
+//        lastImageIdx = returned_image;
 
-        gBufferLength = width;
+        if(imaging){
+            success = axGetImageInfo(session, 0, &info);
+            if(success != AxErr::NO_AxERROR) {
+                logAxErrorVerbose(__LINE__, success);
+            } else {
+                const uint32_t output_buf_len{SectorHeight_px * SectorWidth_px};
+                prefs.request_mode = AxRequestMode::RETRIEVE_TO_CALLER;
+                prefs.which_window = 0;
+                success = axRequestImage(session, info.image_number, prefs, output_buf_len, axsunData->acqData, &info);
+            }
+        }
+
+        gBufferLength = info.width;
 
         // write in frame information for recording/playback
         axsunData->frameCount = gDaqCounter;
@@ -345,8 +369,8 @@ bool DAQ::getData( )
 
         return true;
     }
-    return false;
-}
+//    return false;
+//}
 
 /*
  * startDaq
@@ -355,12 +379,12 @@ bool DAQ::getData( )
  */
 bool DAQ::startDaq()
 {
-    AxErr success = NO_AxERROR;
+    AxErr success = AxErr::NO_AxERROR;
 
     try {
 
         success = axStartSession(&session, 50);    // Start Axsun engine session
-        if(success != NO_AxERROR){
+        if(success != AxErr::NO_AxERROR){
             logAxErrorVerbose(__LINE__, success);
         }
         const int framesUntilForceTrig {35};
@@ -371,18 +395,18 @@ bool DAQ::startDaq()
          */
         msleep(100);
         success = axSetTrigTimeout(session, framesUntilForceTrig * 2);
-        if(success != NO_AxERROR){
+        if(success != AxErr::NO_AxERROR){
             logAxErrorVerbose(__LINE__, success);
         }
 
         msleep(100);
         success = axSelectInterface(session, AxInterface::GIGABIT_ETHERNET);
-        if(success != NO_AxERROR){
+        if(success != AxErr::NO_AxERROR){
             logAxErrorVerbose(__LINE__, success);
         }
         msleep(100);
         success = axGetMessage(session, axMessage );
-        if(success != NO_AxERROR){
+        if(success != AxErr::NO_AxERROR){
             logAxErrorVerbose(__LINE__, success);
         }
         LOG1(axMessage)
@@ -392,7 +416,7 @@ bool DAQ::startDaq()
         LOG1(__FUNCTION__)
     }
 
-    return success == NO_AxERROR;
+    return success == AxErr::NO_AxERROR;
 }
 
 /*
@@ -403,13 +427,13 @@ bool DAQ::startDaq()
 bool DAQ::shutdownDaq()
 {
     qDebug() << "***** DAQ::shutdownDaq()";
-    AxErr success = NO_AxERROR;
+    AxErr success = AxErr::NO_AxERROR;
 
     success = axStopSession(session);    // Stop Axsun engine session
-    if(success != NO_AxERROR){
+    if(success != AxErr::NO_AxERROR){
         logAxErrorVerbose(__LINE__, success);
     }
-    return success == NO_AxERROR;
+    return success == AxErr::NO_AxERROR;
 }
 
 void DAQ::setLaserDivider()
@@ -420,11 +444,11 @@ void DAQ::setLaserDivider()
         {
             LOG1(subsamplingFactor)
             AxErr success = axSetSubsamplingFactor(subsamplingFactor,0);
-            if(success != NO_AxERROR){
+            if(success != AxErr::NO_AxERROR){
                 logAxErrorVerbose(__LINE__, success);
             }
             success = axGetMessage( session, axMessage );
-            if(success != NO_AxERROR){
+            if(success != AxErr::NO_AxERROR){
                 logAxErrorVerbose(__LINE__, success);
             }
         }
