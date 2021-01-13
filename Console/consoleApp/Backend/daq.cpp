b57e4765c99e44b8b4666957d9264af60b967fda
Laszlo Vincze <lvincze@avinger.local>
2021-01-13 09:28:52 -0800

    0.2.13223 - LOG2(counter, counterImage0);

--- Console/consoleApp/Backend/daq.cpp
+++ Console/consoleApp/Backend/daq.cpp
@@ -266,6 +266,7 @@ bool DAQ::getData( )
     request_prefs_t prefs{ };
     image_info_t info{ };
     static int32_t counter = 0;
+    static int32_t counterImage0 = 0;
 
     // get Main Image Buffer status
     AxErr success = axGetStatus(session, &imaging, &last_packet, &last_frame, &last_image, &dropped_packets, &frames_since_sync);
@@ -385,19 +386,21 @@ bool DAQ::getData( )
                 prefs.which_window = 0;
                 success = axRequestImage(session, info.image_number, prefs, output_buf_len, axsunData->acqData, &info);
                 int currentImageNumber = info.image_number;
-//                if(sprevReturnedImageNumber != currentImageNumber){
-                    sprevReturnedImageNumber = currentImageNumber;
-                    isNewData = true;
-                    gBufferLength = info.width;
+                if(!currentImageNumber){
+                    ++counterImage0;
+                }
+                if(counter % 100 == 0){
+                    LOG2(counter, counterImage0);
+                }
+                isNewData = true;
+                gBufferLength = info.width;
 
-                    // write in frame information for recording/playback
-                    axsunData->frameCount = gDaqCounter;
-                    axsunData->timeStamp = fileTimer.elapsed();;
-                    axsunData->milliseconds = 30;
+                // write in frame information for recording/playback
+                axsunData->frameCount = gDaqCounter;
+                axsunData->timeStamp = fileTimer.elapsed();;
+                axsunData->milliseconds = 30;
 
-                    gDaqCounter++;
-//                }
-//                LOG3(isNewData, sprevReturnedImageNumber, currentImageNumber)
+                gDaqCounter++;
             }
         }
 
