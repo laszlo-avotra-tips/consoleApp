40e909b720073ab449ed4fe8bf0498af10cbfbbc
Laszlo Vincze <lvincze@avinger.local>
2021-01-20 02:33:10 -0800

    0.2.13603 - refactoring

--- Console/consoleApp/Frontend/Widgets/mainScreen.cpp
+++ Console/consoleApp/Frontend/Widgets/mainScreen.cpp
@@ -68,7 +68,7 @@ MainScreen::MainScreen(QWidget *parent)
 
     m_scene = new liveScene( this );
     m_graphicsView->setScene(m_scene);
-    DisplayManager::instance()->getGraphicsView()->setScene(m_scene);
+//    DisplayManager::instance()->getGraphicsView()->setScene(m_scene);
     m_scene->handleReticleBrightnessChanged();
 
     m_graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
