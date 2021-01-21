c129a5ee7ff3692cf060ea885ef49967fa301064
Laszlo Vincze <lvincze@avinger.local>
2021-01-21 04:19:55 -0800

    0.2.13709 - +//        memcpy(m_clipBuffer,...

--- Console/consoleApp/Frontend/Widgets/mainScreen.cpp
+++ Console/consoleApp/Frontend/Widgets/mainScreen.cpp
@@ -732,16 +732,16 @@ void MainScreen::updateSector(OCTFile::OctData_t *frameData)
 //        LOG2(image->width(),image->height());
 //        LOG2(image->sizeInBytes(), image->bytesPerLine());
 
-        frameData->dispData = image->bits();
+//        frameData->dispData = image->bits();
         auto bufferLength = sm->getBufferLength();
 
 //        LOG2(image->width(),image->height())
         m_scanWorker->warpData( frameData, bufferLength);
 
-        OCTFile::OctData_t clipData(*frameData);
-        clipData.dispData = m_clipBuffer;
+//        OCTFile::OctData_t clipData(*frameData);
+//        clipData.dispData = m_clipBuffer;
 //        m_scanWorker->warpData( &clipData, bufferLength);
-        memcpy(m_clipBuffer,frameData->dispData,1024*1024);
+//        memcpy(m_clipBuffer,frameData->dispData,1024*1024);
 
         if(m_scanWorker->isReady){
 
@@ -773,7 +773,7 @@ void MainScreen::updateSector(OCTFile::OctData_t *frameData)
                 const QString catheterName{names[0]};
                 const QString cathalogName{names[1]};
 
-                emit updateRecorder(clipData.dispData,
+                emit updateRecorder(frameData->dispData,
                                     catheterName.toLatin1(),cathalogName.toLatin1(),
                                     activePassiveValue.toLatin1(),
                                     timeLabel.toLatin1(),
