9a88dd187e202aa1f4085c87fcda58c2d28e5028
Laszlo Vincze <lvincze@avinger.local>
2021-01-21 10:18:29 -0800

    13730 - displayFrame

--- Console/consoleApp/Backend/daq.cpp
+++ Console/consoleApp/Backend/daq.cpp
@@ -181,8 +181,10 @@ void DAQ::run( void )
                 if( newData )
                 {
                     gFrameNumber = ++loopCount % NUM_OF_FRAME_BUFFERS;
+
+                    int displayFrame = (gFrameNumber + 4) % NUM_OF_FRAME_BUFFERS;
                     auto* sm =  SignalModel::instance();
-                    OCTFile::OctData_t* axsunData = sm->getOctData(gFrameNumber);
+                    OCTFile::OctData_t* axsunData = sm->getOctData(displayFrame);
                     LOG3(axsunData->frameCount, axsunData->acqData, axsunData->timeStamp);
 //                    gFrameNumber = ++loopCount % NUM_OF_FRAME_BUFFERS;
 
