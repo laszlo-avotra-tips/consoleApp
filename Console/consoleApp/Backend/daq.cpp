527f4d83e0673c64c9b6167978150a33234c07ef
Laszlo Vincze <lvincze@avinger.local>
2021-02-04 06:06:01 -0800

    0.2.14305 - decimate LOG1(msg)

--- Console/consoleApp/Backend/daq.cpp
+++ Console/consoleApp/Backend/daq.cpp
@@ -422,7 +422,12 @@ bool DAQ::getData(new_image_callback_data_t data)
     else
         qs << "Memory allocation too small for retrieval of image " << data.image_number;
 
-    LOG1(msg);
+//    LOG1(msg);
+    if(!data.image_number)
+        LOG1(msg);
+
+    if(data.image_number && (data.image_number % 16 == 0))
+        LOG1(msg);
 
     return true;
 }
