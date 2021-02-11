b39b44e69a47135737ecf29c88c0105af5b93786
Laszlo Vincze <lvincze@avinger.local>
2021-02-11 12:27:39 -0800

    0.2.1477 - enable m_scene->paintOverlay();

--- Console/consoleApp/Frontend/Widgets/mainScreen.cpp
+++ Console/consoleApp/Frontend/Widgets/mainScreen.cpp
@@ -799,7 +799,7 @@ void MainScreen::updateSector(OCTFile::OctData_t *frameData)
                        if(pixmap && !m_disableRendering){
                            const QPixmap& tmpPixmap = QPixmap::fromImage( *image, Qt::MonoOnly);
                            pixmap->setPixmap(tmpPixmap);
-//                           m_scene->paintOverlay();
+                           m_scene->paintOverlay();
                        }
                    }
                }
