5a1c6b69d6761c1820b11182cc8c82ab896a484f
Laszlo Vincze <lvincze@avinger.local>
2021-02-21 13:38:45 -0800

    0.2.15224 - video player label fix

--- Console/consoleApp/Frontend/Widgets/videoplayer.cpp
+++ Console/consoleApp/Frontend/Widgets/videoplayer.cpp
@@ -69,6 +69,9 @@ void VideoPlayer::init()
     m_mediaPlayer = new QMediaPlayer(this,QMediaPlayer::VideoSurface);
     m_videoWidget = new QVideoWidget();
 
+    m_errorLabel = new QLabel(this);
+    m_errorLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
+
     if(!m_videoWidgetContainer){
         m_videoWidgetContainer = new QVBoxLayout();
         setLayout(m_videoWidgetContainer);
@@ -77,6 +80,7 @@ void VideoPlayer::init()
     }
     QVBoxLayout *layout = m_videoWidgetContainer;
     layout->addWidget(m_videoWidget);
+    layout->addWidget(m_errorLabel);
 
     m_videoWidget->autoFillBackground();
     m_mediaPlayer->setVideoOutput(m_videoWidget);
@@ -99,6 +103,7 @@ VideoPlayer::~VideoPlayer()
     LOG1("~VideoPlayer");
     delete m_videoWidget;
     delete m_mediaPlayer;
+    delete m_errorLabel;
 }
 
 
