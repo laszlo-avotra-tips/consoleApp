5e8394a748abe99e0d0e112683e9cc97aa4b0b5c
Laszlo Vincze <lvincze@avinger.local>
2021-02-22 09:22:01 -0800

    0.2.15233 - >labelRunTime->move(1000,100);

--- Console/consoleApp/Frontend/Widgets/formPmCaseReview.cpp
+++ Console/consoleApp/Frontend/Widgets/formPmCaseReview.cpp
@@ -2,6 +2,7 @@
 #include "ui_formPmCaseReview.h"
 
 #include <QGraphicsScene>
+#include <QLabel>
 
 FormPmCaseReview::FormPmCaseReview(QWidget *parent) :
     QWidget(parent),
@@ -19,6 +20,7 @@ void FormPmCaseReview::setScene(QGraphicsScene* scene)
 {
     ui->captureScene->setScene(scene);
     ui->pushButtonLogo->hide();
+    ui->labelRunTime->move(1000,100);
 }
 
 void FormPmCaseReview::showCapture(bool isVisible)
