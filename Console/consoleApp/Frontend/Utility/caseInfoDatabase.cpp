e5bcbe03b627213288e48d5431c2e53caedcb765
Laszlo Vincze <lvincze@avinger.local>
2021-02-26 08:33:14 -0800

    0.2.15353

--- Console/consoleApp/Frontend/Utility/caseInfoDatabase.cpp
+++ Console/consoleApp/Frontend/Utility/caseInfoDatabase.cpp
@@ -166,6 +166,29 @@ void CaseInfoDatabase::initCaseInfo()
     QSqlError sqlerr;
     QStringList nl;
 
+    int maxID = 0;
+    q.prepare( "SELECT MAX(id) FROM Locations" );
+
+    q.exec();
+    sqlerr = q.lastError();
+    if(sqlerr.isValid()){
+        const QString& errorMsg = sqlerr.databaseText();
+        LOG1(errorMsg)
+        return;
+    }
+    q.next();
+    QSqlRecord record = q.record();
+    if( record.isNull("MAX(id)") )
+    {
+        maxID = 1;
+    }
+    else
+    {
+        int idCol = record.indexOf( "MAX(id)" );
+        maxID = record.value( idCol ).toInt();
+    }
+    LOG1(maxID);
+
     q.prepare( "SELECT name FROM Locations" );
 
     q.exec();
@@ -174,7 +197,16 @@ void CaseInfoDatabase::initCaseInfo()
         const QString& errorMsg = sqlerr.databaseText();
         LOG1(errorMsg)
     }
-    QSqlRecord record = q.record();
-
+    record = q.record();
     LOG1(record.count());
+
+    int numLocations{0};
+    LOG1(record.value(0).toString());
+    LOG1(record.indexOf("name"));
+    while(q.next()){
+        ++numLocations;
+        record = q.record();
+        LOG1(record.value(0).toString());
+    }
+    LOG1(numLocations);
 }
