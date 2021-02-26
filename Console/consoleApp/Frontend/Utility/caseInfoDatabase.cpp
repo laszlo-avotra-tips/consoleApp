d6ae47d8599d2438d786c337d45c9ba80fa47ef7
Laszlo Vincze <lvincze@avinger.local>
2021-02-26 05:15:41 -0800

    0.2.15346

--- Console/consoleApp/Frontend/Utility/caseInfoDatabase.cpp
+++ Console/consoleApp/Frontend/Utility/caseInfoDatabase.cpp
@@ -18,7 +18,42 @@ QSqlError CaseInfoDatabase::initDb()
         sqlerr =  db.lastError();
         const QString& errorMsg = sqlerr.databaseText();
         LOG1(errorMsg)
+        return sqlerr;
     }
+    // Setup the database only in the case
+    // that the tables don't already exist
+    QStringList tables = db.tables();
+
+    // Set up the schema version table
+    if( !tables.contains( "version", Qt::CaseInsensitive ) )
+    {
+        // Doesn't exist, create it.
+        QSqlQuery q;
+        if( !q.exec(QLatin1String("create table version(id integer primary key, tablename varchar, version integer)") ) )
+        {
+            sqlerr = q.lastError();
+            const QString& errorMsg = sqlerr.databaseText();
+            LOG1(errorMsg)
+            return sqlerr;
+        }
+    }
+
+    // Set up the Captures table
+    if( !tables.contains( "captures", Qt::CaseInsensitive ) )
+    {
+
+        // Doesn't exist, create it.
+        QSqlQuery q;
+        if( !q.exec(QLatin1String("create table captures(id integer primary key, timestamp timestamp, tag varchar, name varchar, deviceName varchar, pixelsPerMm int)") ) )
+        {
+            sqlerr = q.lastError();
+            const QString& errorMsg = sqlerr.databaseText();
+            LOG1(errorMsg)
+            return sqlerr;
+        }
+    }
+
+    LOG1(sqlerr.databaseText())
 
     return sqlerr;
 }
