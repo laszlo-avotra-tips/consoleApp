f727606f4bb63f5a0d70e43ee1cdc89869abc0a1
Laszlo Vincze <lvincze@avinger.local>
2021-04-13 01:29:52 -0700

    0.2.16194 - revert simulation buffer use

--- Console/consoleApp/Backend/signalmodel.cpp
+++ Console/consoleApp/Backend/signalmodel.cpp
@@ -24,7 +24,7 @@ void SignalModel::allocateOctData()
 
     int frameBufferCount = userSettings::Instance().getNumberOfDaqBuffers();
 
-    for(int i = 0; i <= frameBufferCount; ++i){
+    for(int i = 0; i < frameBufferCount; ++i){
 
         OCTFile::OctData_t oct;
 
@@ -341,7 +341,6 @@ OctData SignalModel::handleSimulationSettings(OctData &od)
     const bool isSequencial = settings.getIsSequencial();
     const int  sequenceLimitH = settings.getSequenceLimitH();
     const int  sequenceLimitL = settings.getSequenceLimitL();
-    const int  numberOfDaqBuffers = settings.getNumberOfDaqBuffers();
 
     if(isSimulation){
         if(isRecording){
@@ -357,7 +356,7 @@ OctData SignalModel::handleSimulationSettings(OctData &od)
                 m_simulationFrameCount = sequenceLimitL;
             }
             od.frameCount = m_simulationFrameCount++;
-            od.acqData = getOctData(numberOfDaqBuffers)->acqData; //od.acqData;
+            od.acqData = od.acqData;
             retrieveOct(od);
         }
     }
