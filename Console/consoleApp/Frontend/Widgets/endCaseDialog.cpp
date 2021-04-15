9d3f65b3022a19142ff8a9230517bfa6748211cf
Laszlo Vincze <lvincze@avinger.local>
2021-04-15 14:04:51 -0700

    0.2.16508 - animation

--- Console/consoleApp/Frontend/Widgets/endCaseDialog.cpp
+++ Console/consoleApp/Frontend/Widgets/endCaseDialog.cpp
@@ -1,5 +1,8 @@
 #include "endCaseDialog.h"
 #include "ui_endCaseDialog.h"
+#include <QGraphicsOpacityEffect>
+#include <QPropertyAnimation>
+#include <QParallelAnimationGroup>
 
 endCaseDialog::endCaseDialog(QWidget *parent) :
     QDialog(parent),
@@ -7,6 +10,30 @@ endCaseDialog::endCaseDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     setWindowFlags(Qt::SplashScreen);
+
+    /*
+     * Set opacity with animation
+     */
+    const int animationDuration_ms=1000;
+    const QByteArray property{"opacity"};
+    const float startValue{0.0f};
+    const float endValue{0.9f};
+
+    QGraphicsOpacityEffect * showing_effect = new QGraphicsOpacityEffect(this);
+    QPropertyAnimation* animation = new QPropertyAnimation(showing_effect, property);
+    QParallelAnimationGroup *group = new QParallelAnimationGroup(this);
+
+
+    setGraphicsEffect(showing_effect);
+    animation->setStartValue(startValue);
+    animation->setEndValue(endValue);
+    animation->setDuration(animationDuration_ms);
+    group->addAnimation(animation);
+    group->start();
+
+    setWindowFlags( windowFlags() & Qt::CustomizeWindowHint );
+    setWindowFlags( windowFlags() & ~Qt::WindowTitleHint );
+
 }
 
 endCaseDialog::~endCaseDialog()
