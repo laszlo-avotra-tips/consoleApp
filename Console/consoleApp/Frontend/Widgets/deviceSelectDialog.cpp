7b09f85674ba632875074c15d20edfe0706d4b3f
Laszlo Vincze <lvincze@avinger.local>
2021-05-03 12:06:36 -0700

    0.2.16808 - QueuedConnection

--- Console/consoleApp/Frontend/Widgets/deviceSelectDialog.cpp
+++ Console/consoleApp/Frontend/Widgets/deviceSelectDialog.cpp
@@ -148,7 +148,7 @@ void DeviceSelectDialog::startDaq(MainScreen *ms)
     if(idaq){
         if( idaq->startDaq() )
         {
-            const bool isUpdateSectorConnected = connect( idaq, &IDAQ::updateSector, ms, &MainScreen::updateSector, Qt::BlockingQueuedConnection);
+            const bool isUpdateSectorConnected = connect( idaq, &IDAQ::updateSector, ms, &MainScreen::updateSector, Qt::QueuedConnection);
             LOG1(isUpdateSectorConnected);
             idaq->initDaq();
         } else {
