d3dbb8b33888ca59cec8e268768253330feda356
Laszlo Vincze <lvincze@avinger.local>
2021-05-08 03:00:16 -0700

    0.2.16954 - time.elapsed()

--- Console/consoleApp/Frontend/Utility/displayThread.cpp
+++ Console/consoleApp/Frontend/Utility/displayThread.cpp
@@ -1,6 +1,7 @@
 #include "displayThread.h"
 #include "logger.h"
 #include "signalmodel.h"
+#include <QElapsedTimer>
 
 DisplayThread::DisplayThread()
 {
@@ -11,6 +12,8 @@ void DisplayThread::run()
 {
     auto sm = SignalModel::instance();
     int index{-1};
+    QElapsedTimer time;
+    time.start();
     while(true){
         yieldCurrentThread();
         msleep(1);
@@ -18,7 +21,9 @@ void DisplayThread::run()
         yieldCurrentThread();
         if(sm->renderingQueueIndex() > 0 && index != sm->renderingQueueIndex()){
             index = sm->renderingQueueIndex();
-            LOG3(m_count, sm->renderingQueueIndex(), priority());
+            auto deltaT = time.elapsed();
+            LOG4(m_count, sm->renderingQueueIndex(), priority(), deltaT);
+            time.restart();
         }
     }
 }
