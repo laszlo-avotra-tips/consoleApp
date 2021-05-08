743994662cebf142329604414eba3918bfcdf5c8
Laszlo Vincze <lvincze@avinger.local>
2021-05-08 02:42:21 -0700

    Update displayThread.cpp

--- Console/consoleApp/Frontend/Utility/displayThread.cpp
+++ Console/consoleApp/Frontend/Utility/displayThread.cpp
@@ -10,9 +10,15 @@ DisplayThread::DisplayThread()
 void DisplayThread::run()
 {
     auto sm = SignalModel::instance();
+    int index{-1};
     while(true){
-        sleep(1);
+        yieldCurrentThread();
+        msleep(1);
         ++m_count;
-        LOG2(m_count, sm->renderingQueueIndex());
+        yieldCurrentThread();
+        if(index != sm->renderingQueueIndex()){
+            index = sm->renderingQueueIndex();
+            LOG2(m_count, sm->renderingQueueIndex());
+        }
     }
 }
