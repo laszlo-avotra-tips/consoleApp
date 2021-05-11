06d404d54fbe781818b60cf63073650ae64df8fc
Laszlo Vincze <lvincze@avinger.local>
2021-05-11 02:13:53 -0700

    0.2.17078 - BlockingQueuedConnection

--- Console/consoleApp/Frontend/Widgets/mainScreen.cpp
+++ Console/consoleApp/Frontend/Widgets/mainScreen.cpp
@@ -65,7 +65,7 @@ MainScreen::MainScreen(QWidget *parent)
     connect(ui->pushButtonMedium, &QPushButton::clicked, this, &MainScreen::udpateToSpeed2);
     connect(ui->pushButtonHigh, &QPushButton::clicked, this, &MainScreen::udpateToSpeed3);
     connect(this, &MainScreen::sledRunningStateChanged, this, &MainScreen::handleSledRunningState);
-    connect(&m_daqTimer, &QTimer::timeout, this, &MainScreen::updateImage);
+    connect(&m_daqTimer, &QTimer::timeout, this, &MainScreen::updateImage, Qt::BlockingQueuedConnection);
 
     QMatrix matrix = ui->graphicsView->matrix();
     ui->graphicsView->setTransform( QTransform::fromScale( IMAGE_SCALE_FACTOR * matrix.m11(), IMAGE_SCALE_FACTOR * matrix.m22() ) );
